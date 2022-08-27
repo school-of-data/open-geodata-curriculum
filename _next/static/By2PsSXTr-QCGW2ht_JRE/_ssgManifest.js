b558a95928500daaf45eeebc28329cb3c3b7a636
benhur07b <benhur07b@users.noreply.github.com>
2022-08-27 08:58:30 +0000

    Deploying to gh-pages from @ school-of-data/open-geodata-curriculum@f85c1d8fbb9a85fe06d6e83c7531f8e64dafc3c4 🚀

---
+++ _next/static/By2PsSXTr-QCGW2ht_JRE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[locale]\u002F[country]","\u002F[locale]\u002F[country]\u002Fmodule\u002F[slug]","\u002F[locale]","\u002F[locale]\u002Fshowcase"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
