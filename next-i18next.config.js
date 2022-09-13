571f6eefc1a2aa85951b4f81e610bd9a3215dba5
benhur07b <benhur07b@users.noreply.github.com>
2022-09-14 07:46:28 +0800

    added ethiopia (english) localization

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'ar', 'es'],
     defaultCountry: 'philippines',
-    countries: ['kenya', 'mexico', 'philippines', 'united-arab-emirates',]
+    countries: ['ethiopia', 'kenya', 'mexico', 'philippines', 'united-arab-emirates',]
   },
   reloadOnPrerender: true
 }
