d49a6321ba125641a966f4a62b3bc711363f87dc
benhur07b <benhur07b@users.noreply.github.com>
2022-09-14 05:18:51 +0800

    added kenya localization

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'ar', 'es'],
     defaultCountry: 'philippines',
-    countries: ['philippines', 'united-arab-emirates', 'mexico']
+    countries: ['kenya', 'mexico', 'philippines', 'united-arab-emirates',]
   },
   reloadOnPrerender: true
 }
