2819ccc7861cba52a54d5151e216343c91c5664f
nikeshbalami <nikeshbalami@users.noreply.github.com>
2022-11-11 10:06:31 +0000

    Deploying to gh-pages from @ school-of-data/open-geodata-curriculum@fcefc5748ca56e521cb48f1a3669dd835421138a 🚀

---
+++ _next/static/re5itu4Ym9WOs_NqM3zR5/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[locale]\u002F[country]","\u002F[locale]\u002F[country]\u002Fmodule\u002F[slug]","\u002F[locale]\u002Fcasestudies","\u002F[locale]","\u002F[locale]\u002Fshowcase","\u002F[locale]\u002Ftutorial"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
